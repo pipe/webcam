e3942dcf60016c8cb12d94eddce81a9b8fba4260
Tim Panton <tim@pi.pe>
2021-03-02 11:43:09 +0000

    Use JDK 11 unless you can't

--- install.sh
+++ install.sh
@@ -2,9 +2,19 @@
 ## enable camera
 ## reboot
 
+
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y galternatives openjdk-8-jre-headless
+
+JAVANUM='11'
+if [ `uname -m` = "armv6l" ]
+then
+  JAVANUM='8'
+fi
+JDK=`printf "openjdk-%s-jre-headless" $JAVANUM`
+echo JDK to install is  $JDK
+
+sudo apt-get install -y galternatives $JDK
 
 sudo apt-get install -y libgstreamer1.0-0 gstreamer1.0-plugins-base \
    gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
